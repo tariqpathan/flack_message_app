b9744d4d6f0da232722cf7e95a62662e2d5da494
Tariq Pathan <tariq047@gmail.com>
2020-04-24 18:58:27 +0100

    removed namespace

--- static/script.js
+++ static/script.js
@@ -11,7 +11,7 @@ var infoTimeout = 3000; // time in ms that info messages are displayed
 var socket;
 
 function connectSocketIO () {
-    socket = io.connect('https://' + document.domain + ':' + location.port + namespace);
+    socket = io.connect('https://' + document.domain + ':' + location.port);
     socket.on('connect', () => {
         console.log('client connected');
         socket.on('disconnect', () => console.log('client disconnected'));
